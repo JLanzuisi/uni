2e75b9b8a78b798bf8fe8dffa0bec148fe4bafd8
Jhonny Lanzuisi <jalb97@gmail.com>
2021-05-19 10:09:24 -0400

    Add files of first algo2 lab

---
+++ abr-jul-2021/algo2lab/lab1/Sortlib.kt
@@ -0,0 +1,28 @@
+// Sortlib.kt implements functions for 4 sorting algorithms
+// Copyright (C) 2021  Jhonny Lanzuisi
+
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+
+fun bubbleSort(A: Array<Int>) {
+ // Realizar!
+}
+
+fun insertionSort(A: Array<Int>) {
+ // Realizar!
+}
+
+fun selectionSort(A: Array<Int>) {
+ // Realizar!
+}
+
+fun shellSort(A: Array<Int>) {
+ // Realizar!
+}
