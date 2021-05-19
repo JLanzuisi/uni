820e47bcf685c916e60407a6dc271dc41ba403b3
Jhonny Lanzuisi <jalb97@gmail.com>
2021-05-19 15:11:01 -0400

    Add license header

--- abr-jul-2021/algo2lab/lab1/runSortlib.sh
+++ abr-jul-2021/algo2lab/lab1/runSortlib.sh
@@ -1,3 +1,15 @@
 #!/usr/bin/bash
+# runSortlib.sh shell script wrapper around Main.kt & Sortlib.kt
+# Copyright (C) 2021  Jhonny Lanzuisi
+
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
 
 # Realizar!
