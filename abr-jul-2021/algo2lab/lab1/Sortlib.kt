8dd8a44c3212a66c0bbdd126b4e0436ed9b226cf
Jhonny Lanzuisi <jalb97@gmail.com>
2021-05-20 16:54:43 -0400

    copyright header & implement bubbleSort

--- abr-jul-2021/algo2lab/lab1/Sortlib.kt
+++ abr-jul-2021/algo2lab/lab1/Sortlib.kt
@@ -1,28 +1,42 @@
-// Sortlib.kt implements functions for 4 sorting algorithms
-// Copyright (C) 2021  Jhonny Lanzuisi
+/*
+ * Main.kt test and benchmark sorting algorithms
+ * Copyright (C) 2021  Jhonny Lanzuisi
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ */
 
-// This program is free software: you can redistribute it and/or modify
-// it under the terms of the GNU General Public License as published by
-// the Free Software Foundation, either version 3 of the License, or
-// (at your option) any later version.
-
-// This program is distributed in the hope that it will be useful,
-// but WITHOUT ANY WARRANTY; without even the implied warranty of
-// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-// GNU General Public License for more details.
-
-fun bubbleSort(A: Array<Int>) {
- // Realizar!
+fun bubbleSort(A: Array<Int>): Array<Int> {
+    var B = A
+    val N = B.size
+    for (i in 0..N-2){
+        for (j in N-1 downTo i+1){
+            if (B[j] < B[j-1]){
+                // Swap the values
+                var temp = B[j]
+                B[j] = B[j-1]
+                B[j-1] = temp
+            }
+        }
+    }
+    return B
 }
 
-fun insertionSort(A: Array<Int>) {
- // Realizar!
-}
+// fun insertionSort(A: Array<Int>): Array<Int> {
+ 
+// }
 
-fun selectionSort(A: Array<Int>) {
- // Realizar!
-}
+// fun selectionSort(A: Array<Int>) {
+//  // Realizar!
+// }
 
-fun shellSort(A: Array<Int>) {
- // Realizar!
-}
+// fun shellSort(A: Array<Int>) {
+//  // Realizar!
+// }
