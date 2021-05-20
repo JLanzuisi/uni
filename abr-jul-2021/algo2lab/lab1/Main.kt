55c8abd955a80d0ef55d21015c4e2b33047d78d3
Jhonny Lanzuisi <jalb97@gmail.com>
2021-05-20 16:55:36 -0400

    Copyright header & test calling functions in sortlib

--- abr-jul-2021/algo2lab/lab1/Main.kt
+++ abr-jul-2021/algo2lab/lab1/Main.kt
@@ -1,16 +1,22 @@
-// Main.kt test and benchmark sorting algorithms
-// Copyright (C) 2021  Jhonny Lanzuisi
-
-// This program is free software: you can redistribute it and/or modify
-// it under the terms of the GNU General Public License as published by
-// the Free Software Foundation, either version 3 of the License, or
-// (at your option) any later version.
-
-// This program is distributed in the hope that it will be useful,
-// but WITHOUT ANY WARRANTY; without even the implied warranty of
-// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-// GNU General Public License for more details.
+/*
+ * Main.kt test and benchmark sorting algorithms
+ * Copyright (C) 2021  Jhonny Lanzuisi
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ */
 
 fun main(args: Array<String>) {
-    // Realizar
+    val A = arrayOf(5,4,7)
+    val B = bubbleSort(A)
+    for (i in B){
+        println(i)
+    }
 }
