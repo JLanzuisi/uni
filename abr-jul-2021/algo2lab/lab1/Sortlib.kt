74bfa705080a545fb2f9510b069108d6d5f4625f
Jhonny Lanzuisi <jalb97@gmail.com>
2021-05-20 17:00:29 -0400

    fix typo in header

--- abr-jul-2021/algo2lab/lab1/Sortlib.kt
+++ abr-jul-2021/algo2lab/lab1/Sortlib.kt
@@ -1,5 +1,5 @@
 /*
- * Main.kt test and benchmark sorting algorithms
+ * Sortlib.kt implement 4 sorting algorithms
  * Copyright (C) 2021  Jhonny Lanzuisi
  *
  * This program is free software: you can redistribute it and/or modify
